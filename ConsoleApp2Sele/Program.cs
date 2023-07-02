0af97101c3ec5a6d4381683039bb0e7b2b06c2d7
Mahendra K <techno2mahi@gmail.com>
2023-07-03 02:38:07 +0530

    Initial commit working

---
+++ ConsoleApp2Sele/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumDocs.GettingStarted
+{
+    [TestCase]
+    public class FirstScriptTest
+    {
+
+        [TestMethod]
+        public void ChromeSession()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.selenium.dev/selenium/web/web-form.html");
+
+            var title = driver.Title;
+            Assert.AreEqual("Web form", title);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+
+            var textBox = driver.FindElement(By.Name("my-text"));
+            var submitButton = driver.FindElement(By.TagName("button"));
+
+            textBox.SendKeys("Selenium");
+            submitButton.Click();
+
+            var message = driver.FindElement(By.Id("message"));
+            var value = message.Text;
+            Assert.AreEqual("Received!", value);
+
+            driver.Quit();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Selenium101
+
+Steps:
+
+Create a .net core MSTest unit test application 
+
+Copy the sample code  as mentioned in the selenium documentation 
+
+Install this in Package manager console 
+ >   Install-Package Selenium.WebDriver
+
+The project now  build successfully 
+
+Now install the  the driver chrome driver 
+	   In Nuget package manager search for this package 
+	 > Selenium.WebDriver.ChromeDriver  
+	I have tried this version 114.0.5735.9000
